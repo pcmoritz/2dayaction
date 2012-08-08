c57326a1a9c18a0cebb216329ac1989a733b684f
bijanc <bijan@gmx.de>
2012-08-09 00:20:51 +0200

    bijan

--- direction2.cc
+++ direction2.cc
@@ -1,55 +1,28 @@
 #include <iostream>
 #include "field.hh"
 using namespace std;
-pos unitvec[6] = {{1,1},{0,1},{-1,0},{-1,-1},{0,-1},{1,0}};
 
 bool check(pos a, pos b, pos tmp, pos dir, field &f);
 
-bool is_equal(pos a, pos b) {
-	if(a.a==b.a&& a.b ==b.b) return true;
-	else return false;
-}
-
-pos add_pos(pos a, pos b){
-	pos c;
-	c.a = a.a+b.a;
-	c.b = a.b+b.b;
-	return c;
-}
-pos sub_pos(pos a, pos b){
-	pos c;
-	c.a = a.a-b.a;
-	c.b = a.b-b.b;
-	return c;
-}
-pos inv_dir(pos a){
-	pos c;
-	c.a = -a.a;
-	c.b = -a.b;
-	return c;
-}
-
-bool move_valid(pos a, pos dir,field &f) {
-	if(f.get_stone(add_pos(a,dir).a,add_pos(a,dir).b)) return false;
-	else return true;
-}
 
 bool move_valid(pos a, pos b, pos c, pos dir, field &f) {
 	if(f.get_stone(a.a,a.b)==f.get_stone(b.a,b.b) 
-      and f.get_stone(c.a,c.b)==f.get_stone(a.a,a.b)){
-  dist=sub_pos(a,b);
+      and f.get_stone(c.a,c.b)==f.get_stone(a.a,a.b)){ // Same colour
+  pos dist=sub_pos(a,b);
   if(is_equal(unitvec[0],dist) or is_equal(unitvec[1],dist) or 
      is_equal(unitvec[2],dist) or is_equal(unitvec[3],dist)
   or is_equal(unitvec[4],dist) or is_equal(unitvec[5],dist)){
     if ( is_equal(c, add_pos(a,dist) ) or is_equal(c, sub_pos(b,dist) ) ) {
-		  if(is_equal(dist,dir) or is_equal(dist,inv_dir(dir)) ) {
-      // Parallel, c is in front
+        // 3 in a row
+		  if(is_equal(dist,dir) or is_equal(dist,inv_dir(dir)) ) { 
+        // c is in front
         if ( is_equal(sub_pos(c,dir),a) or is_equal(sub_pos(c,dir),b) ) {
 				cout<<"c is in front"<<endl;
 				pos tmp = add_pos(c,dir);
         return check(a,b,tmp,dir,f);
 			  }
         else{
+        // c not in front
 				pos tmp = add_pos(c,dir);
         tmp = add_pos(c,dir);
         tmp = add_pos(c,dir);
@@ -58,18 +31,27 @@ bool move_valid(pos a, pos b, pos c, pos dir, field &f) {
       }
       else {
       // NonParallel
+   			cout<<"NPARALLEL"<<endl;
+				pos tmp = add_pos(a,dir);
+				pos tmp2 = add_pos(b,dir);
+				pos tmp3 = add_pos(c,dir);
+				if(f.get_stone(tmp.a,tmp.b) or f.get_stone(tmp2.a,tmp2.b) or 
+            f.get_stone(tmp3.a,tmp3.b) ) return false;
+				else return true;
+			}
+//			else return false;
+    
       }
     }
-  } 
   dist=sub_pos(b,c);
   if(is_equal(unitvec[0],dist) or is_equal(unitvec[1],dist) or 
      is_equal(unitvec[2],dist) or is_equal(unitvec[3],dist)
   or is_equal(unitvec[4],dist) or is_equal(unitvec[5],dist)){
-    if ( is_equal(a, sub_pos(b,dist) ) or is_equal(a, add_pos(a,dist) )   ) {
+    if ( is_equal(a, add_pos(b,dist) ) or is_equal(a, sub_pos(c,dist) )   ) {
 		  if(is_equal(dist,dir) or is_equal(dist,inv_dir(dir)) ) {
-      // Parallel, c is in front
-        if ( is_equal(sub_pos(c,dir),a) or is_equal(sub_pos(c,dir),b) ) {
-				cout<<"c is in front"<<endl;
+      // Parallel, a is in front
+        if ( is_equal(sub_pos(a,dir),b) or is_equal(sub_pos(a,dir),a) ) {
+				cout<<"a is in front"<<endl;
 				pos tmp = add_pos(c,dir);
         return check(a,b,tmp,dir,f);
 			  }
@@ -82,12 +64,23 @@ bool move_valid(pos a, pos b, pos c, pos dir, field &f) {
       }
       else {
       // NonParallel
+   			cout<<"NPARALLEL"<<endl;
+				pos tmp = add_pos(a,dir);
+				pos tmp2 = add_pos(b,dir);
+				pos tmp3 = add_pos(c,dir);
+				if(f.get_stone(tmp.a,tmp.b) or f.get_stone(tmp2.a,tmp2.b) or 
+            f.get_stone(tmp3.a,tmp3.b) ) return false;
+				else return true;
       }
     }
   }
 
-  }
+  } 
   else return false;
+}
+
+int main(){
+//unitvec[6] = {{1,1},{0,1},{-1,0},{-1,-1},{0,-1},{1,0}};
 }
 
 bool check(pos a, pos b, pos tmp, pos dir, field &f){
